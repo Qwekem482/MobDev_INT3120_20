15e6d575e6b0139376ffc3e699a8d9b532bd493c
Phùng Ánh Dương <Qwekem4802@gmail.com>
2023-10-05 00:56:10 +0700

    Slide 10

---
+++ Slide10/DemoProvider/app/src/main/java/com/example/demoprovider/DemoProvider.java
@@ -0,0 +1,118 @@
+package com.example.demoprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+import java.util.HashMap;
+
+public class DemoProvider extends ContentProvider {
+    static final String PROVIDER_NAME = "com.demo.user.provider";
+    static final String URL = "content://" + PROVIDER_NAME + "/users";
+
+    static final Uri CONTENT_URI = Uri.parse(URL);
+
+    static final String id = "id";
+    static final String name = "name";
+    static final int uriCode = 1;
+    static final UriMatcher uriMatcher;
+    private static HashMap<String, String> values;
+
+    private SQLiteDatabase database;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(PROVIDER_NAME, "users", uriCode);
+        uriMatcher.addURI(PROVIDER_NAME, "users/*", uriCode);
+    }
+
+    public DemoProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                count = database.delete(DatabaseHelper.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+        // Implement this to handle requests to delete one or more rows.
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (uriMatcher.match(uri)) {
+            case uriCode:
+                return "vnd.android.cursor.dir/users";
+            default:
+                throw new IllegalArgumentException("Unsupported URI");
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long rowID = database.insert(DatabaseHelper.TABLE_NAME, "", values);
+        if (rowID > 0) {
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+        throw new SQLiteException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        database = databaseHelper.getWritableDatabase();
+        return database != null;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(DatabaseHelper.TABLE_NAME);
+        if (uriMatcher.match(uri) == uriCode) {
+            queryBuilder.setProjectionMap(values);
+        } else {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        if (sortOrder == null || sortOrder == "") {
+            sortOrder = id;
+        }
+
+        Cursor cursor = queryBuilder.query(database, projection, selection, selectionArgs, null,
+                null, sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        int count = 0;
+        if (uriMatcher.match(uri) == uriCode) {
+            count = database.update(DatabaseHelper.TABLE_NAME, values, selection, selectionArgs);
+        } else {
+            throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+}
